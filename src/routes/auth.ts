0c26b87c4e8d86fdd4170435126684575926a8b6
Thirawit (Pae) <thirawit.k@siametrics.com>
2022-12-08 12:08:47 +0700

    update: cookie

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -34,8 +34,9 @@ router.post('/signin', async (req: Request, res: Response) => {
                 const token = await jwt.sign({ username: user.username, role: user.role }, SECRET);
                 return res
                     .cookie("access_token", token, {
-                        httpOnly: false,
+                        httpOnly: true,
                         secure: process.env.NODE_ENV === "production",
+                        sameSite: "none",
                     })
                     .status(200)
                     .json({ message: "Logged in successfully 😊 👌" });
